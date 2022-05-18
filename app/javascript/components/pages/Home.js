f4c1f899b99b0bf56ddb62484d6056c0ecc3a3da
kmmontes <kmontes4989@gmail.com>
2022-05-18 15:15:12 -0700

    corrected indentation on footer & header.js and no attributes added

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
  class Home extends Component {
   render() {
     return (
-      <>
+    <>
       <h3>Look for your next apartment with us</h3>
       <img src = "https://www.miamiresidence.com/media/com_estateagent/categories/ea_cat_1_hotel_948452852_236.jpg" classname="home"/>
-      </>
-    )}
+    </>
+  )}
 }
 
 export default Home
