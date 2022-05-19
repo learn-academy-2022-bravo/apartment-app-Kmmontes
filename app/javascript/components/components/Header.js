80f2a0e844cc83b5f8fc41fafcff7f9cdfaa0805
kmmontes <kmontes4989@gmail.com>
2022-05-19 12:57:15 -0700

    addded home navigation, apartment listings and ran some tests

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Nav, NavItem } from 'reactstrap'
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
   render() {
@@ -8,32 +9,58 @@ class Header extends Component {
       current_user,
       new_user_route,
       sign_in_route,
-      sign_out_route
-    } = this.props
-    console.log("logged_in:", logged_in)
-    console.log("current_user:", current_user)
+      sign_out_route,
+      //removed apartment_listings_route
+    } = this.props;
+    console.log("logged_in:", logged_in);
+    console.log("current_user:", current_user);
     return (
       <>
-        <h1>Apartments for you!</h1>
+      <h1>Apartments for you!</h1>
         <Nav>
-          {logged_in &&
+          <NavItem>
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+          </NavItem>
+          {logged_in && (
             <NavItem>
-              <a href={sign_out_route} className="nav-link">Sign Out</a>
+              <a href={sign_out_route} className="nav-link">
+                Sign Out
+              </a>
             </NavItem>
-          }
-          {!logged_in &&
+          )}
+          {!logged_in && (
             <NavItem>
-              <a href={sign_in_route} className="nav-link">Sign In</a>
+              <a href={sign_in_route} className="nav-link">
+                Sign In
+              </a>
             </NavItem>
-          }
-          {!logged_in &&
+          )}
+          {!logged_in && (
             <NavItem>
-              <a href={new_user_route} className="nav-link">Sign Up</a>
+              <a href={new_user_route} className="nav-link">
+                Sign Up
+              </a>
             </NavItem>
-          }
-        </Nav>
+          )}
+            <NavItem>
+              <NavLink to="/apartmentindex" className="nav-link">
+                View Apartment Listings 
+              </NavLink>
+            </NavItem>
+        </Nav>  
       </>
     )
   }
 }
 export default Header
+
+
+
+
+
+
+
+
+
--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class ApartmentIndex extends Component {
   render() {
     return (
+      <>
       <h3>ApartmentIndex</h3>
-    )
+    
+      </>
+    );
   }
 }
